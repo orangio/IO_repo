5c3535ca194945d2237e0c8244a509f350cf2447
Burnat Magdalena <magda8055@o2.pl>
2018-01-17 17:33:47 +0100

    Delete CommentManagement.cpp

--- Project1/CommentManagement.cpp
+++
@@ -1,7 +0,0 @@
-#include "CommentManagement.h"
-
-void CommentManagement::addComment(string _description, int _idTask, int _idUser)
-{
-	Comment comment(_description, _idUser, _idTask);
-	commentsList.push_back(comment);
-}
